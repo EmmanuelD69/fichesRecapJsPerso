9d8825522e44af8d4305a8f174b7b115b5e94361
EmmanuelD69 <emmanueldevfr@gmail.com>
2020-07-22 11:38:17 +0200

    Modif Css accueil + ajout contenues

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 /* SELECTEURS */
-const Menu = document.querySelector(".menu");
+const Menu = document.querySelector('.menu');
 
 /* VALEURS IMPORTANTES */
 const htmlChildrenMenu = Menu.children;
@@ -7,32 +7,32 @@ const htmlChildrenMenu = Menu.children;
 
 /* AJOUTS EVENTS */
 for (div of htmlChildrenMenu) {
-  div.addEventListener("click", detectClick);
+	div.addEventListener('click', detectClick);
 }
 
 /* FONCTIONS */
 function detectClick(click) {
-  const target = click.target.className;
-  switch (target) {
-    case "section bases":
-      console.log("BASES JS");
-      window.location.href = "../pages/basesJs.html";
-      break;
-    case "section tableaux":
-      console.log("TABLEAUX");
-      alert("En cours d'intégration!");
-      break;
-    case "section objets":
-      console.log("OBJETS");
-      alert("En cours d'intégration!");
-      break;
-    case "section dom":
-      console.log("DOM");
-      alert("En cours d'intégration!");
-      break;
-  }
+	const target = click.target.className;
+	switch (target) {
+		case 'section bases':
+			console.log('BASES JS');
+			window.location.href = '../pages/basesJs.html';
+			break;
+		case 'section tableaux':
+			console.log('TABLEAUX');
+			window.location.href = '../pages/tableaux.html';
+			break;
+		case 'section objets':
+			console.log('OBJETS');
+			window.location.href = '../pages/objets.html';
+			break;
+		case 'section dom':
+			console.log('DOM');
+			window.location.href = '../pages/dom.html';
+			break;
+	}
 }
 
 console.log(
-  'Bonjour et bienvenue sur mon espace Memo. \nLa partie "notions de base" est la seule accessible pour l\'instant. \nLes autres parties seront rajoutées progressivement. \nBonne lecture!'
+	'Bonjour et bienvenue sur mon espace Memo. \nLa partie "notions de base" est la seule accessible pour l\'instant. \nLes autres parties seront rajoutées progressivement. \nBonne lecture!'
 );
