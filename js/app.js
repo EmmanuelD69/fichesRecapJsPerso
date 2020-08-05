d4f05ac4b4180d126c2393536ba45974a25c24f7
EmmanuelD69 <emmanueldevfr@gmail.com>
2020-08-05 21:06:44 +0200

    Update App.js journal list

--- js/app.js
+++ js/app.js
@@ -36,6 +36,26 @@ function detectClick(click) {
 console.log(
 	"Bonjour et bienvenue sur mon espace Memo. \nJe rajoute du contenu au quotidien donc ne vous étonnez pas de trouver des parties en cours 'intégration\nBonne lecture!"
 );
+console.log('17/07/20: add Notions de base > BOUCLES Part1,');
+console.log('18/07/20: add Notions de base > BOUCLES Part2,');
+console.log('20/07/20: add Notions de base > Notions de scope,');
+console.log('21/07/20: Refonte page d\'accueil + ajout "app.js",');
+console.log('22/07/20: add fichiers objet.html/tableaux.html/dom.html,');
+console.log('22/07/20: add Intro DOM,');
+console.log('22/07/20: add Types de données (primitive/réferents),');
+console.log('23/07/20: add High order function,');
+console.log('23/07/20: add Méthode map(),');
+console.log('23/07/20: add FOOTER > social networks links ,');
+console.log('24/07/20: add Méthode find(),');
+console.log('24/07/20: add Méthode filter(),');
+console.log('25/07/20: add Méthode some(),');
+console.log('25/07/20: add Méthode every(),');
+console.log('25/07/20: add INTRO Objets,');
+console.log('27/07/20: add Méthode sort(),');
+console.log('28/07/20: add Création de tableau à partir de string,');
+console.log('28/07/20: add Modifier tableau sans changer le référent,');
+console.log('29/07/20: add Merge de tableaux,');
+console.log('29/07/20: add Fonction ternaire,');
 console.log('30/07/20: add Notions de base > Opérateur Ternaire,');
 console.log('31/07/20: add Notions de base > Fonction flèche (ES6),');
 console.log('01/08/20: add DOM > Notions de Hoisting - Levage,');
@@ -44,3 +64,4 @@ console.log('03/08/20: add DOM > Cibler un élément du DOM,');
 console.log('03/08/20: add DOM > HTMLCollection vs NodeList,');
 console.log('03/08/20: add Responsive design,');
 console.log("04/08/20: add OBJET > THIS - Qu'est-ce que c'est?,");
+console.log("05/08/20: add OBJET > THIS - Qu'est-ce que c'est?,");
